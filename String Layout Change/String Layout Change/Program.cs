de750e5ad723512eca427d9bafe2e94337a393ce
BorisDombrowski <32637303+BorisDombrowski@users.noreply.github.com>
2021-05-26 19:13:55 +1000

    Project added
    Добавлена заготовка проекта - приложение, которое работает в фоновом режиме

---
+++ String Layout Change/String Layout Change/Program.cs
@@ -0,0 +1,53 @@
+﻿using String_Layout_Change.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace String_Layout_Change
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Context());
+        }
+    }
+
+    public class Context : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+
+        public Context()
+        {
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.AppIcon,
+                ContextMenu = new ContextMenu(new MenuItem[] 
+                {
+                    new MenuItem("Settings", OpenSettings),
+                    new MenuItem("Exit", Exit)                    
+                }),
+                Visible = true
+            };
+        }
+
+        void OpenSettings(object sender, EventArgs e)
+        {
+            SettingsForm.Instance.ShowDialog();
+        }
+
+        void Exit(object sender, EventArgs e)
+        {            
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
---
+++ String Layout Change/String Layout Change/SettingsForm.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+
+namespace String_Layout_Change
+{
+    public partial class SettingsForm : Form
+    {
+        private static SettingsForm instance = null;
+        public static SettingsForm Instance 
+        { 
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new SettingsForm();
+                }
+                return instance;
+            }
+        }
+
+        private SettingsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
